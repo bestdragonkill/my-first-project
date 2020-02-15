8f5d2a192cb4ef7fc7328c6375ea6788b2701aa3
bestdragonkill <61057263+bestdragonkill@users.noreply.github.com>
2020-02-15 22:32:54 +0100

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,17 @@
+
+public class Time {
+	public static void main(String[]arg) {
+		int hour_now = 79200;
+		int minute = 1020;
+		int second = 1;
+		int number_of_seconds_since_midnight = 43200;
+		int minutes_spended_on_this_exercice = 15;
+		System.out.println(number_of_seconds_since_midnight);
+		System.out.println(86400- hour_now );
+		System.out.println(number_of_seconds_since_midnight * 75 / 100);
+		System.out.println(minutes_spended_on_this_exercice);
+		
+		
+				
+	}
+}
