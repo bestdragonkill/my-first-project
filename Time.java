c54ca2cf253d2cceeca1e8d5bf677b6fadaf5024
bestdragonkill <61057263+bestdragonkill@users.noreply.github.com>
2020-02-16 15:58:11 +0100

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,15 +1,16 @@
 
 public class Time {
 	public static void main(String[]arg) {
-		int hour_now = 79200;
-		int minute = 1020;
+		int total_hours_in_a_day = 86400;
+		int hour_now_in_seconds = 79200;
+		int hour_in_minute_in_seconds = 1020;
 		int second = 1;
 		int number_of_seconds_since_midnight = 43200;
-		int minutes_spended_on_this_exercice = 15;
+		int minutes_spended_on_this_exercice_in_minutes = 15;
 		System.out.println(number_of_seconds_since_midnight);
-		System.out.println(86400- hour_now );
+		System.out.println(86400 - hour_now_in_seconds );
 		System.out.println(number_of_seconds_since_midnight * 75 / 100);
-		System.out.println(minutes_spended_on_this_exercice);
+		System.out.println(minutes_spended_on_this_exercice_in_minutes);
 		
 		
 				
