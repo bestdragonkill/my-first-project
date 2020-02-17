02638dc037610709782ce5b3d23e5290d5b3c99b
bestdragonkill <61057263+bestdragonkill@users.noreply.github.com>
2020-02-17 13:01:03 +0100

    Update Time.java

--- Time.java
+++ Time.java
@@ -3,7 +3,7 @@ public class Time {
 	public static void main(String[]arg) {
 		int total_hours_in_a_day = 86400;
 		int hour_now_in_seconds = 79200;
-		int hour_in_minute_in_seconds = 1020;
+		int minute_now_in_seconds = 1020;
 		int second = 1;
 		int number_of_seconds_since_midnight = 43200;
 		int minutes_spended_on_this_exercice_in_minutes = 15;
